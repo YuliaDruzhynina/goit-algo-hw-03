d041c39471e67576b165175c27b8ff2f6fe5efdb
Poshtaklon <152329594+YuliaDruzhynina@users.noreply.github.com>
2024-01-31 09:04:46 +0200

    15line add return

--- goit-algo-hw-03_1_2.py
+++ goit-algo-hw-03_1_2.py
@@ -11,10 +11,13 @@ def get_days_from_today(data):
         return  
    
     days_beetwen = datetime_today.toordinal() - date_object.toordinal()
-    print(days_beetwen)
+    print(f"Days between: {days_beetwen}")
+    return []
 
 get_days_from_today("2024-01-31")  
 get_days_from_today("2024-01-20")  
+
+
 
 
 import random
@@ -22,7 +25,7 @@ def get_numbers_ticket(min: int, max: int, quantity: int):
 
     set_number = set()  
 
-    if min >=1 and max <=1000 and min<quantity<max:            
+    if min >=1 and max <=1000 and quantity > 0 and quantity < max:            
         while len(set_number) != quantity:
                 number = random.randrange(min, max)
                 set_number.add(number)   
@@ -34,7 +37,7 @@ def get_numbers_ticket(min: int, max: int, quantity: int):
     return []
         
      
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+lottery_numbers = get_numbers_ticket(10, 20, 50)
 print("Ваші лотерейні числа:", lottery_numbers)
 
 
